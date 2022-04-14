c18ffec3a1df8dcf07650b5a27bd9d7ad47abb12
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-14 20:07:02 -0300

    Added a better sidebar

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,10 +1,33 @@
+import Image from "next/image"
 import styles from "../../styles/menu/Menu.module.css"
+import { MenuIcon } from "../icons/Icons"
+import Item from "./item/Item"
 
 export default function Menu() {
 
+    const hideMenu = () => {
+        console.log("hiding")
+        document.querySelector("#__next")?.classList.add("menu-hidden")
+    }
+
+    const showMenu = () => {
+        console.log("showing")
+        document.querySelector("#__next")?.classList.remove("menu-hidden")
+    }
+
+    const isMenuVisible = () => {
+        console.log(`status: ${document.querySelectorAll(".menu-hidden#__next").length == 0}\n${document.querySelectorAll(".menu.menu-hide").length}`)
+        return document.querySelectorAll(".menu-hidden#__next").length == 0
+    }
+
     return (
-        <div className={`${styles.menu} menu`}>
-            That's the Menu. Of course.
-        </div>
+        <aside className={`${styles.menu} menu`}>
+            <div className={`${styles["hide-button"]} hide-btn`} onClick={() => isMenuVisible() ? hideMenu() : showMenu()}>
+                <MenuIcon />
+            </div>
+            <h4>Felipe Matheus Flohr</h4>
+            <Image src="/felipe-384.jpg" width={256} height={256} className={styles["portfolio-img"]} />
+            <Item href="/legal" className={styles["items-margin"]}>Aa</Item>
+        </aside>
     )
 }
---
+++ src/pages/legal.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function Legal() {
+    return (
+        <>
+            <Head>
+                <title>Legal!</title>
+            </Head>
+            <h1>Cool page!</h1>
+        </>
+    )
+}
