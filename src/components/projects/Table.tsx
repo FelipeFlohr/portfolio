58d2ff927c4cc98a8235c50fb85bd7edf8b78806
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-18 14:34:13 -0300

    Fixed key and table errors

--- src/components/projects/Table.tsx
+++ src/components/projects/Table.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { getTechIcon, Project } from "../../pages/api/projects"
+import { Project } from "../../pages/api/projects"
 import styles from "../../styles/projects/Table.module.css"
 import TableData from "./table/TableData"
 import TechIcon from "./table/TechIcon"
@@ -14,20 +14,20 @@ export default function ListTable(props: ListTableProps) {
         return props.projects.map((project, i) => {
             return (
                 <TableData
-                key={i}
-                projectName={project.name}
-                projectDesc={project.description}
-                projectLink={project.link}
-                projectTechs={renderTechs(project)}
-                rowColor={i % 2 == 0 ? "light" : "dark"}
-                projectDescClassName={styles.desc} />
+                    key={i}
+                    projectName={project.name}
+                    projectDesc={project.description}
+                    projectLink={project.link}
+                    projectTechs={renderTechs(project)}
+                    rowColor={i % 2 == 0 ? "light" : "dark"}
+                    projectDescClassName={styles.desc} />
             )
         })
     }
 
     const renderTechs = (project: Project) => {
-        const techs = project.technologies.map(tech => (
-            <TechIcon technology={tech} />
+        const techs = project.technologies.map((tech, i) => (
+            <TechIcon technology={tech} key={i} />
         ))
 
         return (
@@ -40,9 +40,11 @@ export default function ListTable(props: ListTableProps) {
     return (
         <table className={styles.table}>
             <thead>
-                <td>Project name:</td>
-                <td className={styles.desc}>Project description:</td>
-                <td>Technologies:</td>
+                <tr>
+                    <th>Project name:</th>
+                    <th className={styles.desc}>Project description:</th>
+                    <th>Technologies:</th>
+                </tr>
             </thead>
             <tbody>
                 {renderContent()}
