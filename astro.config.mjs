9a4528255116bcb82a40d859897cdc04235b930d
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2023-01-04 23:40:31 -0300

    Astro rework

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "http://example.com",
+  integrations: [svelte(), tailwind(), sitemap()]
+});
