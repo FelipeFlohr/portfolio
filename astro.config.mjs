8c528d2c206e45a2121167a036e9ae69a5ac398d
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2023-01-08 02:14:01 -0300

    Sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://example.com",
+  site: "https://portfolio-2flps-24.vercel.app/",
   integrations: [sitemap(), tailwind(), svelte()]
 });
